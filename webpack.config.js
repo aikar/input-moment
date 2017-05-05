a04fea6797f5282f20d33c662a5126f3b4f2ceb3
Daniel Ennis <dennis@icontact.com>
2017-05-05 12:29:30 -0400

    Update build stuff to be much more friendly

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,32 @@
-module.exports = {
-  entry: {
-    'dist/input-moment': './src/input-moment.js',
+const webpack = require('webpack');
+let isProduction = process.env.NODE_ENV === 'production';
+const entry = isProduction ? {
+    'lib/input-moment': './src/input-moment',
+} : {
     'example/bundle': './example/app.js',
-  },
-  output: {
-    path: __dirname,
-    filename: '[name].js',
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-    ]
-  },
-  externals: {
-    'react': 'React',
-    'react-dom': 'ReactDOM',
-    'moment': 'moment',
-  },
-  devtool: 'source-map'
+};
+
+module.exports = {
+    entry: entry,
+    output: {
+        path: __dirname,
+        filename: '[name].js',
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+            {test: /\.json$/, loader: 'json-loader'},
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.less$/, loader: 'style-loader!css-loader!less-loader'},
+        ]
+    },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'moment': 'moment',
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: "example",
+    }
 };
